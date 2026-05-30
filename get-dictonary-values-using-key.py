19c92cfdd0a0d033e64f0e8e2026c98a199885a3
P Balaji <bettertester21@gmail.com>
2026-05-30 15:23:16 +0530

    Created venv,installing packages

---
+++ get-dictonary-values-using-key.py
@@ -0,0 +1,17 @@
+courses = {
+    "js":"JavaScript Basics",
+    "py":"Python Basics",
+    "ds":"Data Science",
+    "wd":"Web Development"
+}
+
+print(courses.get("js", "Course not found" ))
+
+print(courses.get("py", "Course not found" ))
+
+print(courses.get("ds", "Course not found" ))
+
+print(courses.get("wd", "Course not found" ))
+
+print(courses.get("ml", "Course not found" ))
+
